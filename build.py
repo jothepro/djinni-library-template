649a74783ea59e8926d813168ab385482c1e6823
jothepro <github@jothe.pro>
2021-07-03 18:46:27 +0200

    add get_version function to build.py

--- build.py
+++ build.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
 import os
+from conans import tools
 from djinni_build.djinni_build import DjinniBuild
+
+
+def get_version():
+    """tries to determine the library version based on the git tag, and write it to the VERSION file.
+    If no tag can be found, the version is loaded from the VERSION file."""
+    version = ""
+    try:
+        version = tools.Git().run("describe --tags")
+        tools.save("VERSION", version)
+    except:
+        version = tools.load("VERSION")
+    return version[1:]
+
 
 workdir = os.getcwd()
 djinniBuild = DjinniBuild(
@@ -12,7 +26,7 @@ djinniBuild = DjinniBuild(
     windows_target_dir=f'lib/platform/windows',
     android_target='MyDjinniLibrary',
     android_target_dir=f'lib/platform/android',
-    version=open('VERSION', 'rt').read()[1:],
+    version=get_version(),
     android_profile=f'{workdir}/conan/profiles/android',
     macos_profile=f'{workdir}/conan/profiles/macos',
     ios_profile=f'{workdir}/conan/profiles/ios',
