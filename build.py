8b087e3f7f2f5945575cf896b94789935c7c6203
jothepro <github@jothe.pro>
2021-07-04 21:51:53 +0200

    move djinni_build to build_tools folder; update djinni_build;

--- build.py
+++ build.py
@@ -2,28 +2,27 @@
 
 import os
 from conans import tools
-from djinni_build.djinni_build import DjinniBuild
+from build_tools.djinni_build.djinni_build import DjinniBuild
 from build_tools.get_version import get_version
 
-workdir = os.getcwd()
 djinniBuild = DjinniBuild(
-    working_directory=workdir,
+    working_directory=os.getcwd(),
     darwin_target='MyDjinniLibrary',
-    darwin_target_dir=f'lib/platform/darwin',
+    darwin_target_dir='lib/platform/darwin',
     windows_target='MyDjinniLibrary',
-    windows_target_dir=f'lib/platform/windows',
+    windows_target_dir='lib/platform/windows',
     android_target='MyDjinniLibrary',
-    android_target_dir=f'lib/platform/android',
+    android_target_dir='lib/platform/android',
     version=get_version(),
-    android_profile=f'{workdir}/conan/profiles/android',
-    macos_profile=f'{workdir}/conan/profiles/macos',
-    ios_profile=f'{workdir}/conan/profiles/ios',
-    windows_profile=f'{workdir}/conan/profiles/windows',
-    linux_profile=f'{workdir}/conan/profiles/linux',
-    android_project_dir=f'{workdir}/lib/platform/android/package',
+    android_profile='conan/profiles/android',
+    macos_profile='conan/profiles/macos',
+    ios_profile='conan/profiles/ios',
+    windows_profile='conan/profiles/windows',
+    linux_profile='conan/profiles/linux',
+    android_project_dir='lib/platform/android/package',
     android_module_name='MyDjinniLibrary',
-    nupkg_dir=f'{workdir}/lib/platform/windows/package',
+    nupkg_dir='lib/platform/windows/package',
     nupkg_name='MyDjinniLibrary',
-    swiftpackage_dir=f'{workdir}/lib/platform/darwin/package'
+    swiftpackage_dir='lib/platform/darwin/package'
 )
 djinniBuild.main()
