fffe8859d468fa189743584772e4e09413f625be
jothepro <github@jothe.pro>
2022-03-28 22:42:00 +0200

    upgrading template (#1)
    - update djinni_build to new PyPI package
    - migrate Conan to the new "CMakeDeps" and "CMakeToolchain" generators
    - update djinni-cmake
    - update Android Studio project: gradle, kotlin
    - building for Android now uses the Conan android-ndk toolchain
    - update NuGet package template to .NET6.0
    - update Linux build to GCC 11
    - update gh-action Doxygen to v1.9.3
    - update doxygen-awesome-css to v2.0.3
    - improve language switching in documentation website
    - Update Readme

--- build.py
+++ build.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 
-import os
 from conans import tools
-from djinni_build.djinni_build import DjinniBuild
+from djinni_build import DjinniBuild
 
 
 def get_version():
     """tries to determine the library version based on the git tag, and write it to the VERSION file.
     If no tag can be found, the version is loaded from the VERSION file."""
-    version = ""
     try:
         version = tools.Git().run("describe --tags")[1:]
         tools.save("VERSION", version)
@@ -18,24 +16,13 @@ def get_version():
 
 
 djinniBuild = DjinniBuild(
-    working_directory=os.getcwd(),
-    darwin_target='MyDjinniLibrary',
-    darwin_target_dir='lib/platform/darwin',
-    windows_target='MyDjinniLibrary',
-    windows_target_dir='lib/platform/windows',
-    android_target='MyDjinniLibrary',
-    android_target_dir='lib/platform/android',
     version=get_version(),
-    android_profile='conan/profiles/android',
-    macos_profile='conan/profiles/macos',
-    ios_profile='conan/profiles/ios',
-    windows_profile='conan/profiles/windows',
-    linux_profile='conan/profiles/linux',
-    android_project_dir='lib/platform/android/package',
+    darwin_target='MyDjinniLibrary',
+    windows_target='MyDjinniLibrary',
+    android_target='MyDjinniLibrary',
     android_module_name='MyDjinniLibrary',
-    nupkg_dir='lib/platform/windows/package',
+    nupkg_net_version='net6.0',
     nupkg_name='MyDjinniLibrary',
-    swiftpackage_dir='lib/platform/darwin/package',
     conan_user='jothepro',
     conan_channel='release'
 )
--- conanfile.py
+++ conanfile.py
@@ -1,12 +1,11 @@
 from conans import ConanFile, CMake, tools
 
-required_conan_version = ">=1.36"
+required_conan_version = ">=1.44"
 
 
 def get_version():
     """tries to determine the library version based on the git tag, and write it to the VERSION file.
     If no tag can be found, the version is loaded from the VERSION file."""
-    version = ""
     try:
         version = tools.Git().run("describe --tags")[1:]
         tools.save("VERSION", version)
@@ -21,28 +20,20 @@ class MyLibraryConan(ConanFile):
     description = """A basic Djinni C++ library project template using CMake and Conan."""
     settings = "os", "compiler", "build_type", "arch"
     license = "AGPL-3.0-or-later"
-    generators = "cmake_find_package", "cmake_paths"
+    generators = "CMakeDeps", "CMakeToolchain"
     exports = "VERSION"
     exports_sources = "lib/src/*", "lib/include/*", "lib/CMakeLists.txt", "lib/*.djinni", \
                       "lib/platform/*/CMakeLists.txt", "lib/platform/*/src/*", "lib/platform/*/include/*", "test/*", \
                       "cmake/*", "VERSION", "LICENSE", "CMakeLists.txt"
     author = "jothepro"
-    requires = (
-    )
-    build_requires = (
-        "catch2/2.13.4",
-        "djinni-generator/1.2.0"
-    )
+    requires = [("catch2/2.13.4", "private")]
+
+    tool_requires = [
+        "djinni-generator/1.3.0"
+    ]
 
     def build(self):
-        generator = None
-        if tools.is_apple_os(self.settings.os):
-            generator = "Xcode"
-        elif self.settings.os == "Windows":
-            generator = "Visual Studio 16 2019"
-        cmake = CMake(self, generator=generator)
-        if self.settings.os == "Android":
-            cmake.definitions["ANDROID_PLATFORM"] = self.settings.os.api_level
+        cmake = CMake(self)
         if not tools.get_env("CONAN_RUN_TESTS", True):
             cmake.definitions["BUILD_TESTING"] = "OFF"
         cmake.configure()
