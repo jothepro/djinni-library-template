c04e7a904970ba869edc970eec3dcf1ff1021c01
jothepro <github@jothe.pro>
2022-10-06 18:18:03 +0200

    Update djinni_build and djinni-cmake
    - Updated djinni_build to v2.0.0
    - Updated djinni-cmake to v1.1.0
    - Adapted package structure in lib/package/<platform>/package to the new requirements
      by djinni_build
    - Updated github actions to build debug libraries to demonstrate new debugging
      capabilities introduced in djinni_build v2.0.0
    - Moved development documentation from README to separate page
    - Updated doxygen to v.1.9.4
    - Updated doxygen-awesome-css to v2.1.0
    - Update target platform profiles based on my latest learnings
    - pinned XCode version in github action for a reliable build
    - pinned Conan version in requirements.txt for a reliable build

--- build.py
+++ build.py
@@ -1,27 +1,12 @@
 #!/usr/bin/env python3
 
-from conans import tools
 from djinni_build import DjinniBuild
 
-
-def get_version():
-    """tries to determine the library version based on the git tag, and write it to the VERSION file.
-    If no tag can be found, the version is loaded from the VERSION file."""
-    try:
-        version = tools.Git().run("describe --tags")[1:]
-        tools.save("VERSION", version)
-    except:
-        version = tools.load("VERSION")
-    return version
-
-
 djinniBuild = DjinniBuild(
-    version=get_version(),
     darwin_target='MyDjinniLibrary',
     windows_target='MyDjinniLibrary',
     android_target='MyDjinniLibrary',
     android_module_name='MyDjinniLibrary',
-    nupkg_net_version='net6.0',
     nupkg_name='MyDjinniLibrary',
     conan_user='jothepro',
     conan_channel='release'
